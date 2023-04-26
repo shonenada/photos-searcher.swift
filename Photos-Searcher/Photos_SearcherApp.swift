71b925812c4f72dfca2491a9d6874dc9d17c07b9
shonenada <shonenada@gmail.com>
2023-04-26 18:26:01 +0800

    Initial Commit

---
+++ Photos-Searcher/Photos_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_SearcherApp.swift
+//  Photos-Searcher
+//
+//  Created by shonenada on 2023/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Photos_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
